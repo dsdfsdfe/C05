ab533d311c7c0a580773b5f9f1929451aab49026
dsdfsdfe <72729157+dsdfsdfe@users.noreply.github.com>
2020-11-02 02:53:03 +0900

    Update ft_sqrt.c

--- C05/ft_sqrt.c
+++ C05/ft_sqrt.c
@@ -1,35 +1,15 @@
 int		ft_sqrt(int nb)
 {
-	long n;
-
-	if (nb < 0)
-		return (0);
-	if (nb == 1)
-		return (1);
-	n = 1;
-	while (n * n < nb)
-		n++;
-	if (n * n == nb)
-		return (n);
-	return (0);
-}
-
-
-int		ft_sqrt(int nb)
-{
-	long int	i;
+	long int i;
 
 	i = 1;
+	if (nb <= 0)
+		return (0);
 	if (nb == 1)
 		return (1);
-	if (nb == 0)
-		return (0);
 	while (i * i < nb)
-	{
 		i++;
-	}
 	if ((i * i) == nb)
-		return ((int)i);
-	else
-		return (0);
+		return (i);
+	return (0);
 }
