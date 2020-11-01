1cb2b5dc80ca7df19bda91c58a0400c3f21144ba
dsdfsdfe <72729157+dsdfsdfe@users.noreply.github.com>
2020-11-02 02:35:34 +0900

    Update ft_recursive_factorial.c

--- C05/ft_recursive_factorial.c
+++ C05/ft_recursive_factorial.c
@@ -2,18 +2,7 @@ int		ft_recursive_factorial(int nb)
 {
 	if (nb == 2)
 		return (2);
-	if (nb == 1 || nb == 0)
-		return (1);
-	if (nb < 0)
-		return (0);
-	return (ft_recursive_factorial(nb - 1) * nb);
-}
-
-int		ft_recursive_factorial(int nb)
-{
-	if (nb == 1)
-		return (nb);
-	else if (nb == 0)
+	else if (nb == 1 || nb == 0)
 		return (1);
 	else if (nb < 0)
 		return (0);
