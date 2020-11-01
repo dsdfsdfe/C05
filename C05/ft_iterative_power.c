e6812b37f699608ea67bf9e1a48b904b844883dc
dsdfsdfe <72729157+dsdfsdfe@users.noreply.github.com>
2020-11-02 02:44:44 +0900

    Update ft_iterative_power.c

--- C05/ft_iterative_power.c
+++ C05/ft_iterative_power.c
@@ -1,33 +1,18 @@
 int		ft_iterative_power(int nb, int power)
 {
-	int		i;
-	int		res;
+	int	i;
+	int	j;
 
 	i = power;
-	res = 1;
-	if (power < 0)
-		return (0);
-	while (i > 0)
-	{
-		res = res * nb;
-		i--;
-	}
-	return (res);
-}
-
-
-int		ft_iterative_power(int nb, int power)
-{
-	int res;
-	int i;
-
+	j = nb;
 	if (power < 0)
 		return (0);
 	if (power == 0)
 		return (1);
-	res = nb;
-	i = power - 1;
-	while (i--)
-		res *= nb;
-	return (res);
+	while (i > 0)
+	{
+		j *= nb;
+		i--;
+	}
+	return (j);
 }
